b3aa1359768deb56eece4ca1d4e7799f0463f3c5
masoud77 <masoud_rahman@yahoo.com>
2024-06-16 21:04:40 +0530

    Initial commit

---
+++ SudokuSolver/SudokuGrid.py
@@ -0,0 +1,20 @@
+class SudokuCell:
+
+	def __init__ (self, row, col, value):
+		self.row = row
+		self.col = col
+		if value == "_":
+			self.value = list(range(1, 10))
+			self.state = "empty"
+		else:
+			self.value = value
+			self.state = "init"
+
+	def __str__(self):
+		print_str = "SudokuCell Details:\n\tRow: {}\n\tCol: {}\n\tValues: {}\n\tState: {}\n".format(self.row, self.col, self.value, self.state)
+		return print_str
+
+	def __repr__(self):
+		value = "_" if len(self.value) > 1 else self.value
+		repr_str = "SudokuCell({}, {}, \"{}\")".format(self.row, self.col, value)
+		return repr_str
---
+++ SudokuSolver/SudokuSolver.py
@@ -0,0 +1,11 @@
+from SudokuGrid import *
+
+if __name__ == "__main__":
+	sc1 = SudokuCell(6, 5, "7")
+	sc2 = SudokuCell(5, 7, "5")
+	sc3 = SudokuCell(0, 0, "_")
+
+	print("1", sc1)
+	print("2", sc2)
+	print("3", sc3)
+
